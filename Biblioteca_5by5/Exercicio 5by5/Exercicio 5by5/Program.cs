4f1f32b5ce0801150b572410be283a9e506dd757
VitorFaccio1 <53455640+VitorFaccio1@users.noreply.github.com>
2021-03-16 00:09:50 -0300

    Versão final 2.0

--- Biblioteca_5by5/Exercicio 5by5/Exercicio 5by5/Program.cs
+++ Biblioteca_5by5/Exercicio 5by5/Exercicio 5by5/Program.cs
@@ -90,15 +90,14 @@ namespace Exercicio_5by5
         static List<Cliente> CadastrarCliente(List<Cliente> listaClientes)
         {
             //FUNCAO CRIADA PARA FAZER O CADASTRO DE CLIENTES
-            string bairro, logradouro, cidade, estado, cep, telefone, nome, cpf;
+            string bairro, logradouro, cidade, estado, cep, telefone, nome, cpf, resposta;
             DateTime data;
-            long id = 1;
             int contador = 0;
 
             Cliente client = new Cliente();
             Console.WriteLine("Informe o cpf do cliente: ");
-            cpf = Console.ReadLine();
-
+            cpf = Console.ReadLine();        
+   
             //FOREACH PRA PASSAR POR TODA LISTA DE CLIENTES E VERIFICAR SE TEM UM CPF JA CADASTRADO!
             foreach (Cliente clienteCadastrado in listaClientes)
             {
